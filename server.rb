4aa32dee5ab9bb39d37d5f06971b23a8412764eb
Juan Fanals <jfanals@gmail.com>
2022-03-18 10:42:10 +0000

    feat: add authenticaiton, move endpoint to root

--- server.rb
+++ server.rb
@@ -1,25 +1,31 @@
 require 'sinatra'
 require 'handlebars-engine'
 
-get '/api-hbs' do
-  'needs to be post'
+get '/' do
+  halt 405, 'needs to be post'
 end
 
-post '/api-hbs' do
+post '/' do
   rendered = 'SOMETHING WENT WRONG IN THE TEMPLATE'
-  error = ''
+  error = nil
+  body = request.body.read
+
+  halt 401, 'wrong hmac' unless authenticate(body)
 
   begin
-    body = JSON.parse request.body.read
-    logger.info body
+    parsed_body = JSON.parse body
 
-    params = body['params']
-    template = body['template']
+    context = parsed_body['context']
+
+    logger.info 'BODY'
+    logger.info parsed_body
+
+    template = parsed_body['template']
 
     handlebars = Handlebars::Engine.new
     begin
       h_template = handlebars.compile(template)
-      rendered = h_template.call(params)
+      rendered = h_template.call(context)
     rescue StandardError => e
       logger.info e.to_s
       error = e.to_s
@@ -28,12 +34,25 @@ post '/api-hbs' do
     logger.info e.to_s
     error = e.to_s
   end
+
   {
     rendered: rendered,
     error: error
-  }.to_json
+  }.delete_if { |key, value| value == nil }.to_json
 end
 
 not_found do
   '404'
+end
+
+def authenticate(body)
+  secret = ENV['SECRET']
+
+  hmac = request.env["HTTP_X_HMAC_SHA256"]
+
+  halt 401, 'missing hmac' if hmac == nil
+
+  calculated_hmac = Base64.strict_encode64(OpenSSL::HMAC.digest('sha256', secret, body))
+
+  calculated_hmac == hmac
 end
